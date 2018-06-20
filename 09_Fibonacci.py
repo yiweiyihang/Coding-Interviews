6fce5e5342329bf32370a2d7f7e5cbde790b5b7a
yiweiyihang <326187636@qq.com>
2018-06-20 16:16:50 +0800

    Fibonacci变体

--- 09_Fibonacci.py
+++ 09_Fibonacci.py
@@ -19,8 +19,53 @@ class Solution:
             b = temp
         return temp
 
+"""
+题目描述
+一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+"""
 
-
-
+class Solution:
+    def jumpFloor(self, number):
+        a = 1
+        b = 1
+        if(number == 0):return 0
+        if(number == 1): return 1 
+        for i in range(number-1):
+            temp = a + b
+            a = b
+            b = temp
+        return temp
+    
+""" 
+题目描述
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+"""
+# 可用数学归纳法证明 f(n) = 2^n-1
+class Solution:
+    def jumpFloorII(self, number):
+        # write code here
+        res = 1
+        for i in range(number-1):
+            res = res*2
+        return res
+    
+"""
+题目描述
+我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+"""
+class Solution:
+    def rectCover(self, number):
+        # write code here
+        if(number == 0):return 0
+        if(number == 1):return 1
+        if(number == 2): return 2
+        a = 1
+        b = 2
+        for i in range(number-2):
+            res = a + b
+            a = b
+            b = res
+        return res
             
         
