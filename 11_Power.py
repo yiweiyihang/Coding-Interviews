998423faca7d28d0007c3f77d2501584aef12b60
yiweiyihang <326187636@qq.com>
2018-06-23 16:07:05 +0800

    剑指Offer11_数值的整数次方 优化算法

--- 11_Power.py
+++ 11_Power.py
@@ -11,10 +11,10 @@ NOTE:考虑指数为零和负数的情况
 # -*- coding:utf-8 -*-
 BOUND = 1e-6
 invalidInput = False
-class Solution:
+class SolutionA:
     def Power(self, base, exponent):
         # 对非法输入进行处理  
-        if(base<BOUND and exponent<0):
+        if(abs(base)<BOUND and exponent<0):
             global invalidInput
             invalidInput = True
             return -1
@@ -26,3 +26,36 @@ class Solution:
         else:
             # 因为这里res作分母 所以需要考虑res是否为0的情况
             return 1/res
+
+
+"""
+优化：
+n为偶数时 a^n = a^(n/2)*a^(n/2)
+n为奇数时 a^n = a^[(n-1)/2]*a^[(n-1)/2]*a
+"""
+
+class SolutionB:
+    def Power(self,base,exponent):
+        if(exponent == 0):
+            return 1
+        if(exponent == 1):
+            return base
+        # 用右移运算符代替除以2
+        result = self.Power(base,abs(exponent)>>1)
+        result *= result
+        # 用位与运算符代替求余运算符来判断一个数是奇数还是偶数
+        if(exponent & 0x1 == 1):
+            result *= base
+        if(exponent >0):
+            return result
+        else:
+            return 1/result
+
+s = SolutionB()
+print(s.Power(2,3))
+        
+        
+        
+        
+        
+        
